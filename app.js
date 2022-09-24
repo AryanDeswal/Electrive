fdc1c7aeadc4bf44111d29cf136e24835a1c0b7d
Aryan Deswal <aryandeswal870@gmail.com>
2022-09-24 16:18:21 +0530

    Complete contact us form & link with db

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ const studentSchema = new mongoose.Schema({
     year: String
 });
 
+const mailSchema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String,
+    subject: String,
+});
+
 const Student = mongoose.model('Student', studentSchema);
+const Contact = mongoose.model('Contact', mailSchema);
 
 
 app.route('/')
@@ -36,17 +44,17 @@ app.route('/rent')
     })
     .post(function (req, res) {
 
-        const s_name = req.body.name, s_regd_no = req.body.regd_no;
-        const s_roll_no = req.body.roll_no, s_mobile_no = req.body.mobile_no;
-        const s_branch = req.body.branch, s_year = req.body.year;
+        const name = req.body.name, regd_no = req.body.regd_no;
+        const roll_no = req.body.roll_no, mobile_no = req.body.mobile_no;
+        const branch = req.body.branch, year = req.body.year;
 
         const student = new Student({
-            name: s_name,
-            regd_no: s_regd_no,
-            roll_no: s_roll_no,
-            mobile_no: s_mobile_no,
-            branch: s_branch,
-            year: s_year
+            name: name,
+            regd_no: regd_no,
+            roll_no: roll_no,
+            mobile_no: mobile_no,
+            branch: branch,
+            year: year
         });
 
         student.save(function (err) {
@@ -59,6 +67,34 @@ app.route('/rent')
             else { res.send(students); }
         });
     });
+
+
+app.route('/contact')
+    .post(function (req, res) {
+
+        const fname = req.body.fname;
+        const lname = req.body.lname;
+        const email = req.body.email;
+        const subject = req.body.subject;
+        
+        const contact = new Contact({
+            fname: fname,
+            lname: lname,
+            email: email,
+            subject: subject,
+        });
+
+        contact.save(function (err) {
+            if (err) { console.log(err); }
+            else { console.log("Data Inserted"); }
+        });
+
+        Contact.find(function (err, contacts) {
+            if (err) { console.log(err); }
+            else { res.send(contacts); }
+        });
+    });
+
 
 app.listen(3000, function () {
     console.log('Server started successfully,');
